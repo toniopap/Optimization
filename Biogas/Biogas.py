15c770a999fefb9e571f91ebdf0b67d53f60d0f3
toniopap <tonio.paparella@gmail.com>
2025-01-09 08:57:57 +0100

    Update Biogas.py

--- Biogas/Biogas.py
+++ Biogas/Biogas.py
@@ -6,13 +6,15 @@ import csv
 import gamspy as gp
 
 #%% Parameters initialization
+N_limit = 170 # kg N/ha, direttiva nitrati
+N_buffalo = 83 # kg N/ha, Determinazione del carico di azoto secondo il DM 7/4/06
 N_strip	= 0.5
-Land_spred_ha = 1	
-Land_cost = 100
-p_fine = 0.01
-Land_cost_0	= 10
-Fine = 500
-Risk_aversion = 0.5
+Land_spred_ha = 1
+Land_cost = 589 # Euro/ha, Dati RICA, (Affitti passivi / sau in affitto)
+p_fine = 0.3 # Probabilità di sanzione, 
+Land_cost_0	= 0
+Fine = 2582.28 # 516.46 ad euro 2582.28, Disciplina tecnica per la utilizzazione dei liquami zootecnici
+Risk_aversion = 0.193 # Italian farmer risk aversion by elicitation using lotteries, source: https://doi.org/10.1002/aepp.13330
 
 #%% The model
 def u_compliance():
